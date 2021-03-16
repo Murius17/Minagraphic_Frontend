b63adaa3691acc652b7c1c09890f15df22ca5e51
lucasRobert <lucasroberto432@gmail.com>
2021-03-16 12:27:48 -0300

    Limpando código

--- src/components/home/footer.js
+++ src/components/home/footer.js
@@ -7,15 +7,12 @@ export default class Footer extends React.Component {
         <div className="footer">
           <div>
           <img className= "LogoFooter"src='https://w7.pngwing.com/pngs/796/937/png-transparent-triangle-logo-brand-triangle-angle-text-triangle.png' alt='Logo' width="60" height="60"/>
-            MinaGraphic
           </div>
+          <div className="tituloFooter">
+            MinaGraphic
+            </div>
           <div className = "direitosFooter">
           ©2021 MinaGraphic
-          </div>
-          <div className= "contatosFooter"> 
-            Endereço<br/>
-            Telefone<br/>
-            E-mail<br/>
           </div>
         </div>
     );
