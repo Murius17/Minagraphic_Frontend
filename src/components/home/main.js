79ff86ce0beffcb1e86d8ac8ae8240303cde49dd
lucasRobert <lucasroberto432@gmail.com>
2021-03-16 18:07:41 -0300

    grafico aplicado

--- src/components/home/main.js
+++ src/components/home/main.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Cards from "../Main/Cards";
 import Formulario from "../Main/contato"
+import Dados from "../Main/dados";
 
 export default class Main extends React.Component {
   render() {
     return ( 
         <div>
          <Cards/>
+         <Dados/>
          <Formulario/>
          </div>
     );
